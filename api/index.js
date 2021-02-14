66d35ee1e9970d274306276f645f273e84b88261
Saveliy <seva007@hotmail.com>
2021-02-14 22:42:46 +0300

    Add callback info on server start

--- api/index.js
+++ api/index.js
@@ -18,4 +18,4 @@ app.use("/", graphqlHTTP({
     graphiql: true
 }))
 
-app.listen(process.env.PORT || port);
+app.listen(process.env.PORT || port, () => { console.log( "GraphQL API has been started on http://localhost:3008/" ) });
