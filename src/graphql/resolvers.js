e2041056044bd7021ef6169923a2ae3460b134af
Saveliy <seva007@hotmail.com>
2021-03-26 19:48:53 +0300

    da

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -9,7 +9,7 @@ import firebase from "firebase/app";
 import "firebase/database";
 
 // Firebase config
-import firebaseConfig from "../firebase.config"
+import firebaseConfig from "../firebase.config";
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
