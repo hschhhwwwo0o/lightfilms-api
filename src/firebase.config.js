b746e2e9cd4e164355c7c8df68f4452c1d8b59df
Saveliy <seva007@hotmail.com>
2021-03-26 21:24:15 +0300

    Export default firebaseConfig

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const firebaseConfig = {
+export default {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     databaseURL: process.env.FIREBASE_DATABASE_URL,
@@ -10,5 +10,3 @@ const firebaseConfig = {
     appId: process.env.FIREBASE_APP_ID,
     measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
-
-export default firebaseConfig;
