e01531a3c9c064084c637dafaa92fb01cd5a7a78
Saveliy <seva007@hotmail.com>
2021-05-08 11:03:29 +0300

    Correct index.js

--- src/index.js
+++ src/index.js
@@ -18,12 +18,9 @@ const server = new ApolloServer({
 server.applyMiddleware({ app });
 
 app.listen({ port: process.env.PORT || DEV_PORT }, () => {
-    const color = "\x1b[36m%s\x1b[0m";
     console.log("");
 
-    if( process.env.PORT !== undefined ) {
-        console.log(color, `Server has been start on http://localhost:${ process.env.PORT }/graphql`);
-    } else {
-        console.log(color, `Server has been start on http://localhost:${ DEV_PORT }/graphql`);
-    }
+    process.env.PORT !== undefined ?
+    console.log(`Server has been start on http://localhost:${ process.env.PORT }/graphql`) :
+    console.log(`Server has been start on http://localhost:${ DEV_PORT }/graphql`)
 });
