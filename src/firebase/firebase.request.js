7bcda741573444688a0c0c64d2f5eecc2c08e2a9
Saveliy <seva007@hotmail.com>
2021-05-17 12:46:35 +0300

    Update request

--- src/firebase/firebase.request.js
+++ src/firebase/firebase.request.js
@@ -1,38 +1,29 @@
 import database from ".";
 
 export default (child, sort, parametr) => {
-    if(sort === "all" || sort === undefined) {
-        let data;
-        const getData = async () => {
-            await database.ref().child(child).get()
-            .then((s) => { if ( s.exists() ) { data = s.val() }})
-            .catch((error) => { console.error(error) });
-        };
 
-        return getData().then( () => data );
-    } else if(sort === "filter") {
-        let data;
-        const getData = async () => {
-            await database.ref()
-            .child(child).get()
-            .then((s) => { if ( s.exists() ) { data = s.val() }})
-            .catch((error) => { console.error(error) });
-        };
+    let data;
 
-        return getData().then(() => { 
-            return data.filter((p) => { return p.type === parametr });
+    const getData = async (child) => {
+        await database.ref().child(child).get()
+        .then((s) => { if ( s.exists() ) { data = s.val() }})
+        .catch((error) => { console.error(error) });
+    };
+
+    if(sort === "all" || sort === undefined) 
+    {
+        return getData(child).then(() => { return data });
+    } 
+    else if(sort === "filter") 
+    {
+        return getData(child).then(() => { 
+            return data.filter(({ type }) => { return type === parametr });
         });
-    } else if(sort === "find") {
-        let data;
-        const getData = async () => {
-            await database.ref()
-            .child("times").get()
-            .then((s) => { if (s.exists()) { data = s.val() }})
-            .catch((error) => { console.error(error) });
-        };
-
-        return getData().then(() => { 
-            return data.find( ({ id }) => { return id === parametr}) ;
+    } 
+    else if(sort === "find") 
+    {
+        return getData(child).then(() => { 
+            return data.find(({ id }) => { return id === parametr});
         });
-    }
-}
+    };
+};
